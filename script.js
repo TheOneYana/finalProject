69ac731d58469990dc721b7f844abb3f83f44f97
TheOneYana <yanatheone@yahoo.com>
2023-02-04 17:20:12 -0800

    polished code inside html, css, js files

--- script.js
+++ script.js
@@ -50,29 +50,28 @@ name:'book',
 src:'./book.jpg'},
 ]
 
-//creating a set of unique nums to assign to id's inside 'backs' array
+// creating a set of unique nums to assign to id's inside 'backs' array
 const nums = new Set();
 while(nums.size !== backs.length){
  nums.add(Math.floor(Math.random() * backs.length)+1);
 }
-//iterating through the SET to assign random nums to id's inside 'backs' array
+// iterating through the SET to assign random nums to id's inside 'backs' array
 let iterator = [...nums];
 iterator.forEach((randomNum, i)=> {
     backs[i].id = randomNum }
 )
-//assigning class names of nums to all cards
+// selecting all img nodes
 const imgs = document.querySelectorAll('img');
-// for(let i = 1; i<= imgs.length; i++) {
-//     imgs.className = i;
-// }
+
+// creading an array of img nodes
 let iterator2 = [...imgs];
+
 let cardNumsArr = []; 
 let cardsIdsArr =[];
 
-//MAIN FUNCTION
+// MAIN FUNCTION
 function openTwoCards() {
-    let click = 0;
-
+let click = 0;
 window.addEventListener('click', function(event){
     click++;
     console.log(`click = ${click}`)
@@ -80,35 +79,38 @@ let cardNum = (event.target.className);
 // console.log(`card Num: ${cardNum}`)
 const back = backs.find(t => t.id == cardNum);
 // console.log(`back ${back.id}`);
-    event.target.src = back.src;
-    cardsIdsArr.push(back.src);
-    //finds card by its card name between the imgs array
-    const cardClass = iterator2.find(a => a.className == cardNum);
-    cardNumsArr.push(cardClass);
+event.target.src = back.src;
+cardsIdsArr.push(back.src);
+// finds card by its card name between the imgs array
+const cardClass = iterator2.find(a => a.className == cardNum);
+cardNumsArr.push(cardClass);
 
 
-    if(click % 2 == 0 && cardsIdsArr[click-2] !== cardsIdsArr[click-1]) {
-        console.log(`turn over`);
-        if(cardClass.className == cardNum){
+if(click % 2 == 0 && cardsIdsArr[click-2] !== cardsIdsArr[click-1]) {
+    console.log(`turn over`);
+    if(cardClass.className == cardNum){
 // if pictures don't match, they will be turned over after a second.
 this.setTimeout(() => {
     cardNumsArr[click-2].src = './cardSkin.jpg'
     cardNumsArr[click-1].src = './cardSkin.jpg'
 }
- ,"1000");
-//  console.log(`No ${cardClass.className} == ${cardNum}`)
-    }
-    //if a pair of two cards don not have matching pics, turn the cards over
-    } else if (click % 2 == 0 && cardNumsArr[click-2] === cardNumsArr[click-1] && event.target.src !== './cardSkin.jpg'){
-        event.target.src = './cardSkin.jpg'
-    //if a pair of two cards have matching pics,keep showing them.    
-    } else if (click % 2 == 0  && cardsIdsArr[click-2] == cardsIdsArr[click-1]){
+,"1000");
+// console.log(`No ${cardClass.className} == ${cardNum}`)
+}
+    // if a pair of two cards don not have matching pics, turn the cards over
+} else if (click % 2 == 0 && cardNumsArr[click-2] === cardNumsArr[click-1] && event.target.src !== './cardSkin.jpg'){
+    event.target.src = './cardSkin.jpg'
+    // if a pair of two cards have matching pics,keep showing them.    
+} else if (click % 2 == 0  && cardsIdsArr[click-2] == cardsIdsArr[click-1]){
     
-        return console.log(`you guessed right!`)
-    } 
+    return console.log(`you guessed right!`)
+} 
 // console.log(`${event.target.className} clicked`);
 })}
 openTwoCards();
 
 
-// how to count correct number of attempts? if 
+/* to do:
+1. add "Attempts" section where opening two cards would count as 1 attempt
+2. add function that doesn't allow to interact with cards anymore after all cards are open
+3. add 'play' button that reveals all cards for 1 second  */
